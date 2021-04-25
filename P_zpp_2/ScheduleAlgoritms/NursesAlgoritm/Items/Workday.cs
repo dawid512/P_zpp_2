1afdbe0d582dceba593ac0bd12a5c3bd5b13dd95
Samo276 <marek276@gmail.com>
2021-04-25 22:50:49 +0200

    added basic nurses
    bedzie trzeba dodać pola w bazie danych specjalnie dla pielegniarek, inaczej algorytm sie sypnie

---
+++ P_zpp_2/ScheduleAlgoritms/NursesAlgoritm/Items/Workday.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P_zpp_2.ScheduleAlgoritms.NursesAlgoritm.Items
+{
+    public class Workday
+    {
+        public DateTime Date { get; set; }
+
+        List<Nurse> CrewDay { get; set; }
+        List<Nurse> CrewNight { get; set; }
+        List<Nurse> CrewOffStation { get; set; }
+
+        public Workday(DateTime date, List<Nurse> crewDay, List<Nurse> crewNight, List<Nurse> offSiteCrew)
+        {
+            Date = date;
+            CrewDay = crewDay;
+            CrewNight = crewNight;
+            CrewOffStation = offSiteCrew;
+        }
+
+        /*
+        public void PrintAll()
+        {
+            Console.WriteLine("\n---------------------------------\n-----------" + this.Date.Date + "----------\n---------------------------------");
+            PrintDayshift();
+            PrintNightshift();
+            PrintOffStation();
+        }
+        public void PrintDayshift()
+        {
+            Console.WriteLine($"\n-----------Dayshift-----------({this.CrewDay.Count})");
+            foreach (var item in this.CrewDay)
+                item.Print();
+        }
+        public void PrintNightshift()
+        {
+            Console.WriteLine($"\n-----------Nighshift-----------({this.CrewNight.Count})");
+            foreach (var item in this.CrewNight)
+                item.Print();
+        }
+        public void PrintOffStation()
+        {
+            Console.WriteLine($"\n-----------CrewOffStation-----------({ this.CrewOffStation.Count})");
+            foreach (var item in this.CrewOffStation)
+                item.Print();
+        }*/
+    }
+}
---
+++ P_zpp_2/ScheduleAlgoritms/NursesAlgoritm/NursesMain.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace P_zpp_2.ScheduleAlgoritms.NursesAlgoritm.Items
+{
+    public class NursesMain
+    {
+        /*public Task RunNUrsesScheduler(int NumberOfDaysToSchedule)
+        {
+            var PersonelList = new List<Nurse>();
+            try
+            {
+                using (var db = new *//*databasename*//*)
+                {
+                    var personelFromFatabase = db.Roles.Where(x => x.Name == "Nurse").ToList();
+                    foreach (var item in personelFromFatabase)
+                        PersonelList.Add(ToNurseConverter(item));
+                }
+            }
+            catch { }
+            var Schedule = new List<Workday>();
+            for (int i = 0; i < WardWorkInfo.WorkCycleLenght; i++)
+                SelectStaff(Schedule, PersonelList, i);
+        }*/
+
+        /*private Nurse ToNurseConverter(object item)
+        {
+            return new Nurse(
+                item.id,
+                item.name,
+                item.overflowdayshifts,
+                item.overflownightshifts,
+                item.overflowoffdays,
+                CalculateLeave(item)
+                );
+        }*/
+
+        /*private List<DateTime> CalculateLeave(object item)
+        {
+
+            return new List<DateTime> Spisdniwolnych;
+        }*/
+
+        public static void SelectStaff(List<Workday> workdaysList, List<Nurse> Personel, int Nofdays)
+        {
+            var today = DateTime.Now.Date.AddDays(Nofdays);
+            var PeopleWhoHaveLeaveToday = Personel.Where(n => n.AprovedLeave.Contains(today)).ToList();
+            var AvailableStaff = Personel.Except(PeopleWhoHaveLeaveToday).ToList(); // xD
+            var day = SelectForDayShift(AvailableStaff);
+            var night = SelectForNightShift(AvailableStaff, day);
+            var off = SetDayOff(AvailableStaff, day, night);
+
+            workdaysList.Add(new Workday(today, day, night, off));
+            var wd = new Workday(DateTime.Now.Date.AddDays(Nofdays), day, night, off);
+
+        }
+        public static List<Nurse> SelectForDayShift(List<Nurse> Personel)
+        {
+            var tmp = Personel.Where(n => n.DayshiftsCompleted == Personel.Min(m => m.DayshiftsCompleted) && n.LastShift_night != true).Take(WardWorkInfo.Workers_RequiredforDayshift).ToList();
+
+            while (tmp.Count < (WardWorkInfo.Workers_RequiredforDayshift - 1))
+            {
+                tmp.Add(Personel.Except(tmp).Where(n => n.DaysOff == Personel.Except(tmp).Max(m => m.DaysOff) && n.LastShift_night != true).FirstOrDefault());
+            }
+
+            foreach (var item in tmp)
+                item.DayshiftsCompleted++;
+
+            foreach (var item in Personel)
+                item.LastShift_night = false;
+
+            return tmp;
+        }
+        public static List<Nurse> SelectForNightShift(List<Nurse> Personel, List<Nurse> CurrentlyOccupied)
+        {
+            var nonOcupied = Personel.Except(CurrentlyOccupied).ToList();
+            var tmp = nonOcupied.Where(n => n.NightshitCompleted == Personel.Min(m => m.NightshitCompleted)).Take(WardWorkInfo.Workers_RequiredforNightshift).ToList();
+            foreach (var item in tmp)
+            {
+                item.LastShift_night = true;
+                item.NightshitCompleted++;
+            }
+            while (tmp.Count < 2)
+            {
+                tmp.Add(nonOcupied.Except(tmp).Where(n => n.DaysOff == nonOcupied.Except(tmp).Max(m => m.DaysOff)).FirstOrDefault());
+            }
+            return tmp;
+        }
+        public static List<Nurse> SetDayOff(List<Nurse> Personel, List<Nurse> CurrentlyOccupied_Day, List<Nurse> CurrentlyOccupied_night)
+        {
+            var tmp = Personel.Except(CurrentlyOccupied_Day).ToList();
+            var tmp2 = tmp.Except(CurrentlyOccupied_night).ToList();
+
+            foreach (var item in tmp2)
+                item.DaysOff++;
+
+            return tmp2;
+        }
+    }
+}
