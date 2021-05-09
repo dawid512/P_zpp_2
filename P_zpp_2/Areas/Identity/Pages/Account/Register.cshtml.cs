c8ae9e71c3399a2a3933837c6b693074863a5a1f
Dawid <dawid.medrala.98@gmail.com>
2021-05-10 01:26:14 +0200

    rejestration fully working

--- P_zpp_2/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ P_zpp_2/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,7 +46,7 @@ namespace P_zpp_2.Areas.Identity.Pages.Account
 
         public class InputModel
         {
-            
+            [Required]
             [DataType(DataType.Text)]
             [Display(Name = "Typ konta")]
             public string Rola { get; set; }
@@ -90,10 +90,11 @@ namespace P_zpp_2.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { Rola = Input.Rola, UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName };
+                var user = new ApplicationUser { Rola=Input.Rola, UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
+                    _userManager.AddToRoleAsync(user, user.Rola).Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
--- P_zpp_2/Controllers/HomeController.cs
+++ P_zpp_2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using P_zpp_2.Models;
@@ -20,8 +21,9 @@ namespace P_zpp_2.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            
             return View();
         }
 
