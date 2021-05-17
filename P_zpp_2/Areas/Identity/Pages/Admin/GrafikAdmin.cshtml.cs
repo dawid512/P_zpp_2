f906c5bffac81cb9072539f756c6c4abdb157160
Samo276 <marek276@gmail.com>
2021-05-17 09:06:16 +0200

    grafik

--- P_zpp_2/Areas/Identity/Pages/Admin/GrafikAdmin.cshtml.cs
+++ P_zpp_2/Areas/Identity/Pages/Admin/GrafikAdmin.cshtml.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using P_zpp_2.Data;
+using P_zpp_2.Models;
 using P_zpp_2.Models.MyCustomLittleDatabase;
+using P_zpp_2.ScheduleAlgoritms.NursesAlgoritm.Items;
+using static Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal.ExternalLoginModel;
+
 
 namespace P_zpp_2.Areas.Identity.Pages.Admin
 {
@@ -15,6 +20,7 @@ namespace P_zpp_2.Areas.Identity.Pages.Admin
     {
         private readonly P_zpp_2DbContext _context;
         private readonly IConfiguration Configuration;
+        
 
         public GrafikAdminModel(P_zpp_2DbContext context, IConfiguration configuration)
         {
@@ -27,14 +33,35 @@ namespace P_zpp_2.Areas.Identity.Pages.Admin
 
         public void OnGet()
         {
-            _ScheduleDaysList =new List<EventModel>{
-                new EventModel(1, "nornica", "2021-05-16"),
-                 new EventModel(2, "lwiczka", "2021-05-18")
+            _ScheduleDaysList = new List<EventModel>{
+                 new EventModel(1, "2 zmiana", "2021-05-10"),
+                 new EventModel(2, "1 zmiane", "2021-05-12"),
+                 new EventModel(1, "2 zmiana", "2021-05-13"),
+                 new EventModel(2, "1 zmiane", "2021-05-15"),
+                 new EventModel(1, "2 zmiana", "2021-05-16"),
+                 new EventModel(2, "1 zmiane", "2021-05-18"),
+                 new EventModel(2, "2 zmiane", "2021-05-19"),
+                 new EventModel(2, "1 zmiane", "2021-05-21"),
+                 new EventModel(2, "2 zmiane", "2021-05-22"),
+                 new EventModel(2, "1 zmiane", "2021-05-24"),
+                 new EventModel(2, "2 zmiane", "2021-05-25"),
+                 new EventModel(2, "1 zmiane", "2021-05-27"),
+                 new EventModel(2, "2 zmiane", "2021-05-28")
             };
-            var tmp = _ScheduleDaysList;
-            _callMeJson = JsonSerializer.Serialize(tmp);
+
+            //var tmp = DisplayShiftOF(User.Identity.GetUserId(), string ScheduleName);
+
+            //_ScheduleDaysList = 
+
+            
+            _callMeJson = JsonSerializer.Serialize(_ScheduleDaysList);
 
 
         }
+        public List<EventModel> converter(List<SimpleDisplayShifs> toConvertList)
+        {
+            return null;
+        }
+        
     }
 }
