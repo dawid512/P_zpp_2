d8792bafee804793ddc3f3c73092a6d731b572be
Samo276 <marek276@gmail.com>
2021-05-23 19:45:21 +0200

    added placowki to navabr

---
+++ P_zpp_2/Areas/Identity/Pages/Admin/AddFacility.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using P_zpp_2.Data;
+using P_zpp_2.Models;
+using P_zpp_2.Models.MyCustomLittleDatabase;
+using P_zpp_2.ScheduleAlgoritms.NursesAlgoritm.Items;
+using static Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal.ExternalLoginModel;
+
+namespace P_zpp_2.Areas.Identity.Pages.Admin.Pracownicy
+{
+    public class AddFacilityModel : PageModel
+    {
+        private readonly P_zpp_2DbContext _context;
+        private readonly IConfiguration Configuration;
+
+
+        public AddFacilityModel(P_zpp_2DbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            Configuration = configuration;
+        }
+        public void OnGet()
+        {
+        }
+    }
+}
