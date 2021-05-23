7c21fcec90ac7cb05f24d93c0433037b133ce7be
Szymon <szymon.szalata.1@gmail.com>
2021-05-24 00:39:06 +0200

    Fixed navigation
    Pasek nawigacji został naprawiony. Przełączanie się między stronami zostało okodowane. Uporząkowano kod w LoginPartial.

--- P_zpp_2/Controllers/HomeController.cs
+++ P_zpp_2/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace P_zpp_2.Controllers
         {
             return View();
         }
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
