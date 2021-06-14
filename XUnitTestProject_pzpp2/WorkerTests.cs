199dc93313f2888595e628808c4d9daa335f2000
sm2009pl <szymon.mgr99@gmail.com>
2021-06-14 08:11:49 +0200

    tests modified
    tests modified

--- XUnitTestProject_pzpp2/WorkerTests.cs
+++ XUnitTestProject_pzpp2/WorkerTests.cs
@@ -7,7 +7,9 @@ using OpenQA.Selenium.Firefox;
 using P_zpp_2;
 using P_zpp_2.Data;
 using System;
+using System.Linq;
 using System.Net.Http;
+using System.Threading;
 using Xunit;
 using Xunit.Priority;
 
@@ -17,9 +19,11 @@ namespace XUnitTestProject_pzpp2
     public class WorkerTests : IDisposable
     {
         private readonly IWebDriver _driver;
+        private readonly P_zpp_2DbContext _context;
         public WorkerTests()
         {
             _driver = new ChromeDriver();
+            _context = new P_zpp_2DbContext();
         }
 
         public void Dispose()
@@ -87,6 +91,58 @@ namespace XUnitTestProject_pzpp2
         }
 
         [Fact, Priority(3)]
+        public void CreateCommpany()
+        {
+            _driver.Navigate()
+                .GoToUrl("https://localhost:44342/Identity/Account/Login?ReturnUrl=%2F");
+            Login("maciej@gmail.com", "abc123.");
+            _driver.FindElement(By.Id("companiesBtn"))
+                .Click();
+            _driver.FindElement(By.Id("createCompanyBtn"))
+                .Click();
+            _driver.FindElement(By.Id("CompanyName"))
+                .SendKeys("newCompany");
+            _driver.FindElement(By.Id("acceptButton"))
+                .Click();
+            Assert.Contains("newCompany", _driver.PageSource);
+            Logout();
+        }
+        [Fact, Priority(4)]
+        public void CreateDepartment()
+        {
+            _driver.Navigate()
+                .GoToUrl("https://localhost:44342/Identity/Account/Login?ReturnUrl=%2F");
+            Login("maciej@gmail.com", "abc123.");
+            _driver.FindElement(By.Id("departuresBtn"))
+                .Click();
+            _driver.FindElement(By.Id("createNewDep"))
+                .Click();
+            _driver.FindElement(By.Id("DepartureName"))
+                .SendKeys("newDep");
+            _driver.FindElement(By.Id("Companies"))
+                .SendKeys("newCompany");
+            _driver.FindElement(By.Id("acceptButton"))
+                .Click();
+            Assert.Contains("newDep", _driver.PageSource);
+            Logout();
+        }
+        [Fact, Priority(5)]
+        public void SetDepartment()
+        {
+            _driver.Navigate()
+                .GoToUrl("https://localhost:44342/Identity/Account/Login?ReturnUrl=%2F");
+            Login("karol123@gmail.com", "abc123.");
+            string id = _context.Users.Where(x => x.Email == "karol123@gmail.com").Select(x => x.Id).ToString();
+            _driver.Navigate()
+                .GoToUrl(String.Format("https://localhost:44342/Identity/Admin/Pracownicy/Edit?id={0}", id));
+            _driver.FindElement(By.Id("Departures"))
+                .SendKeys("newCompany");
+            _driver.FindElement(By.Id("acceptButton"))
+                .Click();
+            Assert.Contains("newDep", _driver.PageSource);
+            Logout();
+        }
+        [Fact, Priority(6)]
         public void CreateLeave()
         {
             _driver.Navigate()
@@ -94,36 +150,18 @@ namespace XUnitTestProject_pzpp2
             Login("karol123@gmail.com", "abc123.");
             _driver.FindElement(By.Id("LeavesButton"))
                 .Click();
+            Thread.Sleep(500);
             _driver.FindElement(By.Id("SendToModalBtn"))
                 .Click();
-            _driver.FindElement(By.Id("SendToModalBtn"))
-                .Click();
-            _driver.FindElement(By.Id("Leavesname"))
-                .SendKeys("L4");
+            Thread.Sleep(500);
             _driver.FindElement(By.Id("AcceptBtn"))
                 .Click();
+            Thread.Sleep(500);
             Assert.Contains("Status: Oczekuj¹ce", _driver.PageSource);
             Logout();
+          
         }
 
-        [Fact, Priority(4)]
-        public void SetLeaveState()
-        {
-            _driver.Navigate()
-                .GoToUrl("https://localhost:44342/Identity/Account/Login?ReturnUrl=%2F");
-            Login("maciej@gmail.com", "abc123.");
-            _driver.FindElement(By.Id("LeavesButton"))
-                .Click();
-            _driver.FindElement(By.Id("SendToModalBtn"))
-                .Click();
-            _driver.FindElement(By.Id("SendToModalBtn"))
-                .Click();
-            _driver.FindElement(By.Id("Leavesname"))
-                .SendKeys("L4");
-            _driver.FindElement(By.Id("AcceptBtn"))
-                .Click();
-            Assert.Contains("Status: Oczekuj¹ce", _driver.PageSource);
-            Logout();
-        }
+
     }
 }
